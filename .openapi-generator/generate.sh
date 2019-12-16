01633cdc1aa3c0ada0d752b424a8357fb8fd9a08
AntoineDao <antoinedao1@gmail.com>
2019-12-16 12:43:58 +1000

    chore(openapi): update generate.sh to delete all src files before regenerating them
    This avoids keeping deleted resource objects in the package.

--- .openapi-generator/generate.sh
+++ .openapi-generator/generate.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+rm -r build/
+rm -r dist/
+rm -r docs/
+rm -r pollination_sdk/
+rm -r test/
+rm -r pollination_sdk.egg-info/
+
+
 npx openapi-generator generate \
   -i https://api.pollination.cloud/sdk_openapi.json \
   -t .openapi-generator/templates/python \
