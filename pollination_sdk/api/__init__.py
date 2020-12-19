7319568f1029af4a177e324966e3850894f60e2d
AntoineDao <antoinedao1@gmail.com>
2020-12-19 12:06:03 +0000

    upgrade SDK to v0.10.14

--- pollination_sdk/api/__init__.py
+++ pollination_sdk/api/__init__.py
@@ -6,12 +6,12 @@ from __future__ import absolute_import
 from pollination_sdk.api.api_tokens_api import APITokensApi
 from pollination_sdk.api.accounts_api import AccountsApi
 from pollination_sdk.api.artifacts_api import ArtifactsApi
-from pollination_sdk.api.jobs_api import JobsApi
 from pollination_sdk.api.orgs_api import OrgsApi
 from pollination_sdk.api.plugins_api import PluginsApi
 from pollination_sdk.api.projects_api import ProjectsApi
 from pollination_sdk.api.recipes_api import RecipesApi
 from pollination_sdk.api.registries_api import RegistriesApi
+from pollination_sdk.api.runs_api import RunsApi
 from pollination_sdk.api.teams_api import TeamsApi
 from pollination_sdk.api.user_api import UserApi
 from pollination_sdk.api.users_api import UsersApi
